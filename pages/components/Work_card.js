c4dce37b9879d34998abd3606c4b40da0a5a9c20
Tibor Bálint <balinttibornb@gmail.com>
2022-09-16 17:56:29 +0200

    work fix

--- pages/components/Work_card.js
+++ pages/components/Work_card.js
@@ -1,17 +1,16 @@
-import Link from 'next/link';
 import React from 'react';
 import styles from '../../styles/modules/Work.module.scss';
 
 function Work_card({title, year, role, address, link, desc}) {
   return (
     <div className={styles.wrapper}>
-        <Link href={link} target='_blank' rel="noopener noreferrer"><h1 className={styles.title}>
+        <a href={link} target='_blank' rel="noopener noreferrer"><h1 className={styles.title}>
             {title}
-        </h1></Link>
+        </h1></a>
         <h3 className={styles.date}>{year}</h3>
         <h3 className={styles.text}>{role}</h3>
         <h3 className={styles.text}>{address}</h3>
-        <div className={styles.desc}>{desc.split('\n').map((line, idx) => <p key={idx}>{line}</p>)}</div>
+        <div className={styles.desc}>{desc === undefined ? desc.split('\n').map((line, idx) => <p key={idx}>{line}</p>) : ''}</div>
 
     </div>
   )
