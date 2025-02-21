f369cb3bda62160b0efc62672b4902757f6c4ca0
Tibor <balinttibornb@gmail.com>
2025-02-21 17:41:47 +0100

    next config update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: { images: { allowFutureImage: true } }
+  experimental: { images: { allowFutureImage: true } },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...config.externals, 'canvas', 'jsdom']
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig
